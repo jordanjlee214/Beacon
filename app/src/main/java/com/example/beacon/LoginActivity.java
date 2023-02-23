07ee0f9a1018d14d1c0101bc0cea52ab395e98c8
jordanjlee214 <jordanjlee@me.com>
2023-02-23 16:27:39 -0600

    Made a way to access other activites from MainActivity

--- app/src/main/java/com/example/beacon/LoginActivity.java
+++ app/src/main/java/com/example/beacon/LoginActivity.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+
 public class LoginActivity extends AppCompatActivity {
 
+    private BeginSignInRequest signInRequest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //set up objects to enable Google sign-in
+        signInRequest = BeginSignInRequest.builder()
+                .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                        .setSupported(true)
+                        // Your server's client ID, not your Android client ID.
+                        .setServerClientId(getString(R.string.default_web_client_id))
+                        // Only show accounts previously used to sign in.
+                        .setFilterByAuthorizedAccounts(true)
+                        .build())
+                .build();
+
+
     }
 }
--- app/src/main/java/com/example/beacon/MainActivity.java
+++ app/src/main/java/com/example/beacon/MainActivity.java
@@ -2,15 +2,61 @@ package com.example.beacon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private Button eventActivityButton, loginActivityButton, friendActivityButton, mapsActivityButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //set up button objects and connect them to XML layout
+        eventActivityButton = findViewById(R.id.eventactivity_button);
+        loginActivityButton = findViewById(R.id.loginactivity_button);
+        friendActivityButton = findViewById(R.id.friendactivity_button);
+        mapsActivityButton = findViewById(R.id.mapactivity_button);
+
+        //set up listeners for each button
+        //each listener sends user to the corresponding activity
+        eventActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendToActivity(EventActivity.class);
+            }
+        });
+
+        loginActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendToActivity(LoginActivity.class);
+            }
+        });
+
+        friendActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendToActivity(FriendActivity.class);
+            }
+        });
+
+        mapsActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendToActivity(MapsActivity.class);
+            }
+        });
+    }
+
+    private void sendToActivity(Class<?> a) { //this method changes the activity to appropriate activity
+        Intent switchToNewActivity= new Intent(MainActivity.this, a);
+        startActivity(switchToNewActivity);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
     }
 
 
