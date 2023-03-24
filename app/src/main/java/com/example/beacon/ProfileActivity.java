f5c4152d46943a4d5a75435cd9ab58e7f6511466
jordanjlee214 <jordanjlee@me.com>
2023-03-23 21:28:28 -0500

    Created a feature where you can type in a username and visually view their profile. Using ProfileActivity and ProfileFragment.
    Added 2 new APIS: Picasso (displays images from URLs) and a Circular Image View

--- app/src/main/java/com/example/beacon/ProfileActivity.java
+++ app/src/main/java/com/example/beacon/ProfileActivity.java
@@ -1,14 +1,125 @@
 package com.example.beacon;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private ImageView searchButton;
+    private EditText usernameField;
+    private TextView closeButton;
+
+    private FrameLayout fragmentContainer;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference usersRef;
+
+    private boolean fragmentDisplayed;
+
+    private User searchedUser; //the user class representing the user we are trying to find
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        fragmentDisplayed = false;
+        fragmentContainer = (FrameLayout) findViewById(R.id.profileFragmentContainer);
+        searchButton = findViewById(R.id.profileSearchButton);
+        usernameField = findViewById(R.id.searchUsernameField);
+        closeButton = findViewById(R.id.profileExitButton);
+        closeButton.setVisibility(View.GONE);
+
+        mAuth = FirebaseAuth.getInstance();
+        usersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        searchedUser = new User();
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!usernameField.getText().toString().isEmpty()){
+                    if(fragmentDisplayed){
+                        closeFragment();
+                    }
+                    searchUsername(usernameField.getText().toString());
+                }
+            }
+        });
+
+        closeButton.setOnClickListener(new View.OnClickListener() { //this button closes the fragment
+            @Override
+            public void onClick(View view) {
+                if(fragmentDisplayed){
+                    Log.d("PROFILE", "frag: " + fragmentDisplayed);
+                    closeFragment();
+                }
+            }
+        });
+
     }
+
+    /*
+        this helper method searches the database for the entered username and gets an ID
+     */
+    private void searchUsername(String username){
+        usersRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                boolean foundUsername = false;
+                for(DataSnapshot id : snapshot.getChildren()){ //searches through all the user IDs
+                    if(id.child("username").getValue().toString().equals(username)){ //does the username equal this
+                        foundUsername = true; //we found the username!
+                        searchedUser.setUserID(id.getKey().toString()); //set user object's user ID
+                        Log.d("PROFILE", id.getKey().toString());
+                        showProfileFragment(searchedUser.getUserID()); //display the fragment
+                    }
+                }
+                if(!foundUsername){
+                    Toast.makeText(ProfileActivity.this, "Couldn't find that user. :(", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void showProfileFragment(String userID){
+        ProfileFragment profileFragment = ProfileFragment.newInstance(userID);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out, android.R.anim.fade_in, android.R.anim.fade_out);
+        transaction.addToBackStack(null);
+        transaction.add(R.id.profileFragmentContainer, profileFragment, "BLANK_FRAGMENT").commit();
+        closeButton.setVisibility(View.VISIBLE);
+        fragmentDisplayed = true;
+    }
+
+    private void closeFragment(){
+        onBackPressed();
+        fragmentDisplayed = false;
+        closeButton.setVisibility(View.GONE);
+    }
+
 }
--- app/src/main/java/com/example/beacon/ProfileFragment.java
+++ app/src/main/java/com/example/beacon/ProfileFragment.java
@@ -1,12 +1,27 @@
 package com.example.beacon;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.nio.charset.StandardCharsets;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,13 +29,18 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ProfileFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String USERID = "userID";
 
-    // TODO: Rename and change types of parameters
-    private String profileUserID;
+    private String profileUserID; //id of the profile being viewed
+    private String currentUserID; //id of the current user using the app
+
+    private TextView nameDisplay, maleDisplay, femaleDisplay, usernameDisplay, majorDisplay, birthdayDisplay, gradDisplay;
+    private CircleImageView profilePicDisplay;
+
+    private FirebaseAuth mAuth;
+
+    private DatabaseReference usersRef;
+
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -40,14 +60,103 @@ public class ProfileFragment extends Fragment {
             profileUserID = getArguments().getString(USERID);
         }
 
-
-
+        mAuth = FirebaseAuth.getInstance();
+        usersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        currentUserID = mAuth.getCurrentUser().getUid();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+        nameDisplay = v.findViewById(R.id.profileDisplayName_fragment);
+        maleDisplay = v.findViewById(R.id.profileMale_fragment);
+        femaleDisplay = v.findViewById(R.id.profileFemale_fragment);
+        usernameDisplay = v.findViewById(R.id.profileUsername_fragment);
+        majorDisplay = v.findViewById(R.id.profileMajor_fragment);
+        birthdayDisplay = v.findViewById(R.id.profileBirthday_fragment);
+        gradDisplay = v.findViewById(R.id.profileGradYear_fragment);
+        profilePicDisplay = v.findViewById(R.id.profilePicture_fragment);
+
+        maleDisplay.setVisibility(View.GONE);
+        femaleDisplay.setVisibility(View.GONE);
+
+        return v;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        displayUserData();
+    }
+
+    //helper method to display the user data
+    private void displayUserData(){
+        usersRef.child(profileUserID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String firstName = snapshot.child("firstName").getValue().toString();
+                String lastName = snapshot.child("lastName").getValue().toString();
+                String nickname = snapshot.child("nickname").getValue().toString();
+                String birthday = snapshot.child("birthday").getValue().toString();
+                String username = snapshot.child("username").getValue().toString();
+                String gradYear = snapshot.child("graduationYear").getValue().toString();
+                String major = snapshot.child("major").getValue().toString();
+                String gender = snapshot.child("gender").getValue().toString();
+                String photoURL = snapshot.child("photoURL").getValue().toString();
+
+                if(nickname.isEmpty()){
+                    nameDisplay.setText(firstName + " " + lastName);
+                }
+                else{
+                    nameDisplay.setText(firstName + " \"" + nickname + "\" " + lastName);
+                }
+
+                if(gradYear.isEmpty()) {
+                    gradDisplay.setText("Graduation Year Unknown");
+                }
+                else{
+                    gradDisplay.setText("Class of " + gradYear);
+                }
+
+                if(birthday.isEmpty()){
+                    birthdayDisplay.setText("Birthday Unknown");
+                }
+                else{
+                    birthdayDisplay.setText(birthday.substring(0, 2) + "/" + birthday.substring(2, 4) + "/" + birthday.substring(4, 6));
+                }
+
+                if(major.isEmpty()){
+                    majorDisplay.setText("Major Unknown");
+                }
+                else {
+                    majorDisplay.setText(major);
+                }
+
+                usernameDisplay.setText(username);
+                if(gender.equals("M")){
+                    maleDisplay.setVisibility(View.VISIBLE);
+                    femaleDisplay.setVisibility(View.GONE);
+
+                }
+                else if(gender.equals("F")){
+                    femaleDisplay.setVisibility(View.VISIBLE);
+                    maleDisplay.setVisibility(View.GONE);
+                }
+                else{
+                    femaleDisplay.setVisibility(View.GONE);
+                    maleDisplay.setVisibility(View.GONE);
+                }
+
+                //get image from a URL and display it
+                Picasso.get().load(photoURL).placeholder(R.drawable.blank_profile).into(profilePicDisplay);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
