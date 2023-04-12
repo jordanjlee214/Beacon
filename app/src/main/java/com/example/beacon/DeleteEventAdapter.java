47d71c02c177bf127e8e43dea9510d1c6eeffa3e
Tim Wu <tim.wu@my.wheaton.edu>
2023-04-12 04:27:20 -0500

    Worked on Delete Event Page.

---
+++ app/src/main/java/com/example/beacon/DeleteEventAdapter.java
@@ -0,0 +1,54 @@
+package com.example.beacon;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DeleteEventAdapter extends RecyclerView.Adapter<DeleteEventAdapter.ViewHolder> {
+
+    ArrayList<String> data;
+
+    Context context;
+
+    public DeleteEventAdapter(Context context, ArrayList<String> data) {
+        this.context = context;
+        this.data = data;
+    }
+
+    @Override
+    public DeleteEventAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.delete_event_recycler_view_row, parent, false);
+        return new DeleteEventAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        String item = data.get(position);
+        holder.mTextView.setText(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView mTextView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            mTextView = itemView.findViewById(R.id.delete_event_view);
+        }
+    }
+
+}
--- app/src/main/java/com/example/beacon/DeleteEventPage.java
+++ app/src/main/java/com/example/beacon/DeleteEventPage.java
@@ -1,8 +1,109 @@
 package com.example.beacon;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class DeleteEventPage extends AppCompatActivity {
+    private Button  deleteEventButton;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference eventRef;
+
+    private Query query;
+    private ArrayList<String> myEvents;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.delete_event);
+
+        mAuth = FirebaseAuth.getInstance();
+        eventRef = FirebaseDatabase.getInstance().getReference().child("Events");
+        deleteEventButton = findViewById(R.id.deleteEvent_button);
 
 
+        setUpMyEvents();
+        eventRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+/*
+
+                ArrayList<String> myEvents = new ArrayList<>();
+                for (DataSnapshot itemSnapshot : dataSnapshot.getChildren()) {
+                    String item = itemSnapshot.getValue(Event.class).toString();
+                    myEvents.add(item);
+                }
+
+               RecyclerView recyclerView = findViewById(R.id.deleteEventRecyclerView);
+               eventAdapter adapter = new eventAdapter(myEvents);                 recyclerView.setAdapter(adapter);
+               recyclerView.setLayoutManager(new LinearLayoutManager(getParent()));
+*/
+            setUpMyEvents();
+
+          }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+            }
+
+        });
+        RecyclerView recyclerView = findViewById(R.id.deleteEventRecyclerView);
+
+        DeleteEventAdapter adapter = new DeleteEventAdapter(this, myEvents);
+
+        recyclerView.setAdapter(adapter);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+
+
+        deleteEventButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendToActivity(DeleteEventPage.class);
+            }     });
+
+    };
+    private void setUpMyEvents() {
+        query = eventRef.orderByChild("creatorID").equalTo(mAuth.getCurrentUser().getUid());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                ArrayList<String> myEvents = new ArrayList<>();
+                for (DataSnapshot itemSnapshot : dataSnapshot.getChildren()) {
+                    String item = itemSnapshot.getValue(Event.class).toString();
+                    myEvents.add(item);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    };
+
+    private void sendToActivity(Class<?> a) {
+        Intent switchToNewActivity= new Intent(DeleteEventPage.this, a);
+        startActivity(switchToNewActivity);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+    }
 }
